1a7a417e088cb5db81c9c7291afa8d6e62c5602e
Xasbus <davidbilliot0828.gmail.com>
2025-05-27 00:57:00 -0500

    finished TDD 5 tests

---
+++ frontend/src/modules/dataTypes/updateAccount.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { Account } from "./Account";
+import { readAccount } from "./readAccount";
+
+export async function updateAccount(account: Account): Promise<Account> {
+  const { email, password } = account;
+  if (!email || !password) return undefined;
+  const existingUser = await readAccount(account);
+  if (!existingUser || existingUser.password !== password) return undefined;
+  const url = `http://localhost:8000/update`;
+  const data = {
+    email,
+    password,
+    name: "",
+    phone: "",
+  };
+  const readUser = await axios.post(url, data);
+  const loginData = readUser.data;
+  return loginData as Account;
+}
