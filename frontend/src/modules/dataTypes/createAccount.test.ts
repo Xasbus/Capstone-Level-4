310f01d231c578f9b5c83dd7fce23ce744ddb7ce
Xasbus <davidbilliot0828.gmail.com>
2025-06-06 05:09:46 -0500

    frontend commit as insurance

---
+++ frontend/src/modules/dataTypes/createAccount.test.ts
@@ -0,0 +1,86 @@
+import { Account } from "./Account";
+import { createAccount } from "./createAccount";
+import { removeAccount } from "./removeAccount";
+
+describe("createAccount", () => {
+  it("add new email and password into logins table", async () => {
+    //ARRANGE
+    const createUser: Account = {
+      email: "new@email.com",
+      password: "new",
+      name: "Todd",
+      phone: "King",
+    };
+
+    //ACT
+    const result = await createAccount(createUser);
+
+    //ASSERT
+    expect(result).toHaveProperty("email");
+    expect(result).toHaveProperty("password");
+
+    //CLEAN
+    await removeAccount(createUser);
+  });
+
+  it("returns false when given existing email with matching password", async () => {
+    //ARRANGE
+    const createUser: Account = {
+      email: "test@email.com",
+      password: "test",
+      name: "",
+      phone: "",
+    };
+
+    //ACT
+    const result = await createAccount(createUser);
+
+    //ASSERT
+    expect(result).toBe(false);
+  });
+  it("returns false when given empty email", async () => {
+    //ARRANGE
+    const createUser: Account = {
+      email: "",
+      password: "test1",
+      name: "",
+      phone: "",
+    };
+
+    //ACT
+    const result = await createAccount(createUser);
+
+    //ASSERT
+    expect(result).toBe(false);
+  });
+  it("returns false when given existing email, but password is empty", async () => {
+    //ARRANGE
+    const createUser: Account = {
+      email: "test@email.com",
+      password: "",
+      name: "",
+      phone: "",
+    };
+
+    //ACT
+    const result = await createAccount(createUser);
+
+    //ASSERT
+    expect(result).toBe(false);
+  });
+  it("returns false when given email already exists but password doesn't match", async () => {
+    //ARRANGE
+    const createUser: Account = {
+      email: "test@email.com",
+      password: "wrongPassword",
+      name: "",
+      phone: "",
+    };
+
+    //ACT
+    const result = await createAccount(createUser);
+
+    //ASSERT
+    expect(result).toBe(false);
+  });
+});
--- frontend/src/modules/dataTypes/createAccount.ts
+++ frontend/src/modules/dataTypes/createAccount.ts
@@ -2,11 +2,14 @@ import axios from "axios";
 import { Account } from "./Account";
 import { readAccount } from "./readAccount";
 
-export async function createAccount(account: Account): Promise<Account> {
+export async function createAccount(
+  account: Account
+): Promise<Account | boolean> {
   const { email, password, name, phone } = account;
-  if (!email || !password) return undefined;
+  if (!email || !password) return false;
   const existingUser = await readAccount(account);
-  if (existingUser) return undefined;
+  if (existingUser) return false;
+  debugger;
 
   const url = `http://localhost:8000/create`;
   const data = {
@@ -15,7 +18,10 @@ export async function createAccount(account: Account): Promise<Account> {
     name,
     phone,
   };
+  debugger;
   const createUser = await axios.post(url, data);
+  debugger;
   const loginData = createUser.data;
+  debugger;
   return loginData as Account;
 }
